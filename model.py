94f73641f49f9adf92c4f5ab62bde3ebf8329b9b
Yilin Wang <wang2681799845@gmail.com>
2024-04-17 20:45:21 -0400

    added other features and updated README

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ class baseline_model(nn.Module):
         self.lm = lm
         self.args = args
         feature_size =  args.emb_size
-        if args.other_features : feature_size + args.feature_size
+        if args.other_features : feature_size += args.feature_size
         self.head = nn.Linear(feature_size, args.num_labels)
     
     def forward(self, data):
@@ -18,7 +18,7 @@ class baseline_model(nn.Module):
         )
         emb = encoded["pooler_output"]
         if self.args.other_features:
-            emb = torch.cat(emb, data["other_features"], dim = -1)
+            emb = torch.cat((emb, data["other_features"].type(torch.float)), dim = -1)
         logits = self.head(emb)
         return logits
 
@@ -28,6 +28,13 @@ class classification_dataset(Dataset):
         self.args = args
         self.tokenizer = tokenizer
         self.label_2_id = label_2_id
+        self.categories = [
+            'Books_5', 'Clothing_Shoes_and_Jewelry_5', 
+            'Electronics_5','Home_and_Kitchen_5', 'Kindle_Store_5', 
+            'Movies_and_TV_5','Pet_Supplies_5', 'Sports_and_Outdoors_5',
+            'Tools_and_Home_Improvement_5', 'Toys_and_Games_5'
+            ]
+        self.categroy2id = {self.categories[i]: i for i in range(len(self.categories))}
     
     def __len__(self):
         return len(self.df)
@@ -38,7 +45,11 @@ class classification_dataset(Dataset):
                                    truncation = True )
         tokenized["label"] = self.label_2_id[self.df["label"][idx]]
         if self.args.other_features:
-            pass ## TODO here 
+            category = self.df.category[idx]
+            category_id = self.categroy2id[category]
+            category_binary = [1 if i == category_id else 0 for i in range(len(self.categories))]
+            category_binary.append(self.df.rating[idx]) ## add ratings
+            tokenized["other_features"] = category_binary
         for k, v in tokenized.items():
             tokenized[k] = torch.tensor(v)
         return tokenized 
--- run.py
+++ run.py
@@ -7,13 +7,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--emb_size", type=int, default=768)
 parser.add_argument("--bs", type=int, default=8)
 parser.add_argument("--lr", type=float, default=2e-5)
-parser.add_argument("--other_features", type= bool, default=False)
+parser.add_argument("--other_features", action='store_true')
 parser.add_argument("--num_labels", type=int, default=2)
 parser.add_argument("--device", type=str, default="cpu")
 parser.add_argument("--n_epochs", type=int, default=25)  
 parser.add_argument("--train_size", type=float, default=0.7)  
 parser.add_argument("--dev_size", type=float, default=0.1)  
 parser.add_argument("--model_name", type=str, default="roberta-base")  
+parser.add_argument("--feature_size", type=int, default=11)  
 
 
 args = parser.parse_args()
@@ -24,6 +25,7 @@ if torch.cuda.is_available():
 #     args.device = "mps"
 
 if __name__ == "__main__":
+    print(f"Add other features:{args.other_features}")
     test_acc_chosen, dev_accs , test_accs = run_baseline_experiment(args)
     print(f"Test acc is: {test_acc_chosen}")
     print(f"All dev acc {dev_accs}")
